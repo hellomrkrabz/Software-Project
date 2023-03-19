50691d6cf2bd43f25f6177b70f3aff36f5246388
mateuszkasprzyk <mateuszkasprzyk2000@gmail.com>
2023-03-19 12:07:35 +0100

    Testing bootstrap styling

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   );
 
   return (
-    <div>
+    <div className="d-flex flex-column align-items-center">
       {listOfBooks}
     </div>
   );
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+import './../style/bootstrap/css/main_style.css'
 
 function Book(props) {
     return(
-        <div>
-            <div>{props.title}</div>
+        <div className="d-flex flex-column align-items-center bg-secondary bg-gradient mt-2 col-6">
+            <div className="h1">{props.title}</div>
             <div>{props.author}</div>
         </div>
     );
