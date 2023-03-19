6407d9148a371b3c418124c1d4c03d5410524848
Anna Pogorzelska <65682192+hellomrkrabz@users.noreply.github.com>
2023-03-19 10:23:51 +0100

    creating db table :joy_cat:

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
 
 app = Flask(__name__)
+POSTGRESQL_URI = "postgres://rwybqwpr:n0S3V5DoHv3s4MK3n2IZIaYU43LS7mCU@mouse.db.elephantsql.com/rwybqwpr"
+
+connection = psycopg2.connect(POSTGRESQL_URI)
+with connection:
+    with connection.cursor() as cursor:
+        cursor.execute("CREATE TABLE IF NOT EXISTS books (book_id INTEGER, author TEXT, title TEXT);")
 
 
-#db = SQLAlchemy(app)
-#db.init_app(app)
-from model import db, Station
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def main():
- result = Station()
- db.session.add(result)
- #db.session.commit()
- return "Hello"
+    if request.method == "POST":
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO books VALUES (%s, %s, %s);",
+                    (
+                    request.form.get("book_id"),
+                    request.form.get("author"),
+                    request.form.get("title"),
+                    ),
+                )
+    return "Hello"
 
 if __name__ == '__main__':
    app.run()
