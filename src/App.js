a74312e5f41d16c93f55b4280afcdb286d08de0e
Zuucker <72306356+Zuucker@users.noreply.github.com>
2023-03-24 10:15:39 +0100

    front refactor :fish:

--- src/App.js
+++ src/App.js
@@ -7,26 +7,17 @@ function App() {
   const [books, setBooks] = useState(new Array())
 
   useEffect(() => {
-    var tmp=new Array()
-    for(var i=0;i<4;i++)
-    {
-      axios.get("http://localhost:5000/api/bookinfo/"+i).then((response) => {
-        tmp.push(response.data)
-      });
-    }
-    setBooks(tmp)
-  }, []);
 
-  setTimeout(() => {
-    setBooks(books)
-    if(books.length==0)
-      setBooks(books)
-  }, 2500);
+    axios.get("http://localhost:5000/api/book_info").then((response) => {
+      setBooks(response.data.books)
+    });
+
+  }, []);
 
   return (
     <div className="d-flex flex-column align-items-center">
       {books.map((book)=>
-        <Book id={book.book_id} author={book.author} title={book.title}/>
+        <Book {...book}/>
       )}
     </div>
   );
