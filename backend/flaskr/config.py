968b87336afd65fba74375ea01f028cccdf99e66
jakubklimczak <kubaklim2001@gmail.com>
2023-04-03 22:52:52 +0200

    Minor change, but very major when it comes to the backend actually working :ant:

--- backend/flaskr/config.py
+++ backend/flaskr/config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DATABASE_URL = "postgres://banana_books_user:p5KDYaDuvdp5rwHoVyO9bkH2uXkSedzB@dpg-cgljb682qv24jlvodv40-a.frankfurt-postgres.render.com/banana_books"
+DATABASE_URL = "postgresql://banana_books_user:p5KDYaDuvdp5rwHoVyO9bkH2uXkSedzB@dpg-cgljb682qv24jlvodv40-a.frankfurt-postgres.render.com/banana_books"
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
