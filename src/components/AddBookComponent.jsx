97d204a178dcfc92b24526c4694489d6360a0227
Zuucker <72306356+Zuucker@users.noreply.github.com>
2023-04-28 18:00:28 +0200

    added a popup for choosing a book and connected some fields :fire_engine:

--- src/components/AddBookComponent.jsx
+++ src/components/AddBookComponent.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import banana from "../media/banana.png";
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
@@ -7,6 +7,8 @@ import Switch from '@mui/material/Switch'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
+import BookViewer from "./BookViewer";
+import Popup from 'reactjs-popup';
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -52,10 +54,30 @@ function submit() {
     console.log(":D")
 }
 
+function searchBooks()
+{
+    let book1={title:"test1",author:"author1",src:"src1"}
+    let book2={title:"test2",author:"author2",src:"src2"}
+    let book3={title:"test3",author:"author3",src:"src3"}
+    let tmpBooks =[book1,book2,book3]
+    return tmpBooks
+}
+
 
 function AddBookComponent(props)
 {
+
+const [filter, setFilter] = useState("")
 const [condition, setCondition] = useState("good")
+const [isOffered, setIsOffered] = useState(false)
+const [foundBooks, setFoundBooks] = useState([])
+const [book, setBook] = useState({title:"", author:"", src:"", description:""})
+const [hasSearched, setHasSearched] = useState(false)
+
+
+useEffect(() => {
+    console.log(isOffered)
+}, [isOffered]);
 
     return(
         <>
@@ -75,24 +97,45 @@ const [condition, setCondition] = useState("good")
                         <StyledInputBase 
                         placeholder="Find title" 
                         inputProps={{ 'onChange':(e)=>{
-                            setFilter({...filter,"title":e.target.value})
+                            setFilter(e.target.value)
                         } }}/>
                     </Search>
-                    <button>Search</button>
+                    
+                    <button onClick={()=>{
+                        var tmp = searchBooks()
+                        setFoundBooks(tmp)
+                        setHasSearched(true)
+                        }}>Search</button>
+                        
+                    <Popup id="popup" open={hasSearched} position="bottom" onClose={()=>setHasSearched(false)}>
+                        <div onClick={(e)=>{
+                            let value = e.target.getAttribute('value')
+                            setBook({title: value, author: value, src: value, description: value})
+                            setHasSearched(false)
+                            }}>
+                            {foundBooks.map((b)=><p value={b.src}>{b.title}</p>)} {/*zmienic potem value na isbn */}
+                        </div>
+                    </Popup>
                     </div>
                 </div>
                 <div className="row">
                         <img className="col-6 bg-primary" src={banana} height="200px" width="200px" />
                     <div className="col-6 bg-light">
                         <label>Author</label>
-                        <Textfield />
+                        <Textfield 
+                            disabled
+                            value={book.author}/>
                         <label>Title</label>
-                        <Textfield />
+                        <Textfield 
+                            disabled
+                            value={book.title}/>
                     </div>
                 </div>
                 <div className="row d-flex justify-content-center bg-info">
                     <label>Description</label>
                     <Textfield 
+                        disabled
+                        value={book.description}
                         multiline />
                 </div>
                 {props.type==="personal" &&
@@ -114,7 +157,7 @@ const [condition, setCondition] = useState("good")
                             </FormControl>
                         </div>
                         <div className="col-6 bg-warning">
-                            <Switch />
+                            <Switch value={isOffered} onChange={()=>setIsOffered(!isOffered)}/>
                         </div>
                     </div>
                 }
